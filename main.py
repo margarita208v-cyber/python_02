8bcabc9ba589f0e02f830bd0374b0451e94362aa
Elena Gulyaeva <margarita208.v@gmail.com>
2026-09-23 13:11:35 +0300

    Точно последний комит

--- main.py
+++ main.py
@@ -210,12 +210,12 @@ for i2 in range(1,n2+1):
 print(x2)
 
 
-n = int(impunt('Сколько измерений вы хотите: '))
+n = int(input('Сколько измерений вы хотите: '))
 
 total_sum=0
 
 for i in range(1, n + 1):
-    chislo = float(67)
+    chislo = float(input('Ввeдите измерение: '))
     total_sum += chislo
 
 sr_ar = total_sum / n 
